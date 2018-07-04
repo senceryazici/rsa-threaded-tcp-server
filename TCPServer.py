01ac13f1170790895770b898a80283a73d42f714
Sencer Yazici <sencer_yazici98@hotmail.com>
2018-07-04 15:02:30 +0300

    Main TCPServer Created

---
+++ TCPServer.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2017-2018 Sencer Yazici, https://github.com/senceryazici
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from server.socket_server_with_class import BasicChatServer
+import thread
+import serial
+import time
+
+def callback(client, data):
+    # print "[ INFO ] Message Received -> " + data
+    pass
+
+def main():
+    server = BasicChatServer(callback)
+    # port = establish_serial_connection()
+    thread.start_new_thread(server.run, ())
+    while server.ok():
+        # Signal main thread to be opened.
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
